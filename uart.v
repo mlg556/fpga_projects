e774d957c522097f20fa2b4f44bd65af484789b7
Mirac Gulgonul <mlg556@gmail.com>
2023-02-01 15:35:28 +0300

    completed uart example

--- uart.v
+++ uart.v
@@ -1,3 +1,5 @@
+// uart example from https://learn.lushaylabs.com/tang-nano-series/
+// mirac gulgonul - 2023
 `default_nettype none
 
 module uart #(
@@ -12,15 +14,21 @@ module uart #(
 );
 
   localparam HALF_DELAY_WAIT = (DELAY_FRAMES / 2);
+  localparam RX_STATE_IDLE = 0;
+  localparam RX_STATE_START_BIT = 1;
+  localparam RX_STATE_READ_WAIT = 2;
+  localparam RX_STATE_READ = 3;
+  localparam RX_STATE_STOP_BIT = 5;
 
   // receive state machine with 5 states
   // idle, start bit, read wait, read, stop bit
   reg [3:0] rx_state = 0;  // 6 states
   reg [12:0] rx_counter = 0;  // counting clock pulses
   reg [2:0] rx_bit_number = 0;  // how many bits read
-  reg [7:0] dataIn = 0;  // store the byte to be received
+  reg [7:0] data_in = 0;  // store the byte to be received
   reg byte_ready = 0;  // flag, high when data is ready
 
+  // uart receive
   always @(posedge clk) begin
     case (rx_state)  // state machine case switch
 
@@ -41,11 +49,37 @@ module uart #(
       end
 
       RX_STATE_READ_WAIT: begin
-
+        rx_counter <= rx_counter + 1;
+        if ((rx_counter + 1) == DELAY_FRAMES) begin
+          rx_state <= RX_STATE_READ;
+        end
       end
 
-    endcase
+      RX_STATE_READ: begin
+        rx_counter <= 1;
+        // append received to data array
+        data_in <= {uart_rx, data_in[7:1]};
+        rx_bit_number <= rx_bit_number + 1;
+        // try 'd7 instead?
+        if (rx_bit_number == 3'b111) rx_state <= RX_STATE_STOP_BIT;
+        else rx_state <= RX_STATE_READ_WAIT;
+      end
 
+      RX_STATE_STOP_BIT: begin
+        rx_counter <= rx_counter + 1;
+        if ((rx_counter + 1) == DELAY_FRAMES) begin
+          rx_state   <= RX_STATE_IDLE;
+          rx_counter <= 0;
+          byte_ready <= 1;
+        end
+      end
+    endcase
   end
 
+  // display received byte on leds
+  always @(posedge clk) begin
+    if (byte_ready) begin
+      led <= ~data_in[5:0];
+    end
+  end
 endmodule
